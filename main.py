d002684343c45a3f8490c1234c54b388b89df404
Lucas Cantoni <lukcnt@outlook.com>
2022-08-03 18:00:48 -0300

    Added the conditionals to process the user order.

--- main.py
+++ main.py
@@ -1,19 +1,37 @@
-# Greetings
+# Greetings.
 print("Welcome to Maringá Pizza Deliveries!")
 
-# Orders options
+# Orders options.
 size = input("What size pizza do you want? S, M or L: ").lower()
 add_pepperoni = input("Do you want pepperoni? Y or N: ").lower()
 extra_cheese = input("Do you want extra cheese? Y or N: ").lower()
 
-# Prices
+# Prices.
 small_pizza = 15
 medium_pizza = 20
 large_pizza = 25
 pepperoni_small_pizza = 2
 pepperoni_medium_large_pizza = 3
-extra_cheese = 1
+add_cheese = 1
 
-# User bill
+# User bill.
 order = 0
 
+# Processing the user order.
+if size == "s":
+    order += small_pizza
+elif size == "m":
+    order += medium_pizza
+elif size == "l":
+    order += large_pizza
+else:
+    print("That option isn't avaible")
+
+if add_pepperoni == "y":
+    if size == "s":
+        order += pepperoni_small_pizza
+    else:
+        order += pepperoni_medium_large_pizza
+
+if extra_cheese == "y":
+    order += add_cheese
