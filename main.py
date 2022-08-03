ad2438d1d8f720768f57bcb1970c071cd0f3cc35
Lucas Cantoni <lukcnt@outlook.com>
2022-08-03 17:30:50 -0300

    Added the prices of the options.

--- main.py
+++ main.py
@@ -5,3 +5,15 @@ print("Welcome to Maringá Pizza Deliveries!")
 size = input("What size pizza do you want? S, M or L: ").lower()
 add_pepperoni = input("Do you want pepperoni? Y or N: ").lower()
 extra_cheese = input("Do you want extra cheese? Y or N: ").lower()
+
+# Prices
+small_pizza = 15
+medium_pizza = 20
+large_pizza = 25
+pepperoni_small_pizza = 2
+pepperoni_medium_large_pizza = 3
+extra_cheese = 1
+
+# User bill
+order = 0
+
