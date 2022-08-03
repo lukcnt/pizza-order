86a63219a2dfc61f7e90deb629333afb98a07b87
Lucas Cantoni <lukcnt@outlook.com>
2022-08-03 17:24:00 -0300

    Added the options that the client can choose.

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 # Greetings
 print("Welcome to Maringá Pizza Deliveries!")
+
+# Orders options
+size = input("What size pizza do you want? S, M or L: ").lower()
+add_pepperoni = input("Do you want pepperoni? Y or N: ").lower()
+extra_cheese = input("Do you want extra cheese? Y or N: ").lower()
